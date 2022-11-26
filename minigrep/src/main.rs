3b19d004655999accc927ef0fcc32194a3ab769e
mohadangKim <ehdlaos@gmail.com>
2022-11-26 20:32:48 +0900

    minigrep 예제 추가
    - 실행 인자를 전달 받아서 파일 출력

---
+++ minigrep/src/main.rs
@@ -0,0 +1,14 @@
+use std::env;
+use std::fs;
+
+fn main() {
+  let args: Vec<String> = env::args().collect();
+  let query = &args[1];
+  let filename = &args[2];
+  
+  println!("검색어 : {}", query);
+  println!("대상 파일 : {}", filename);
+
+  let contents = fs::read_to_string(filename).expect("파일을 읽지 못했습니다.");
+  println!("파일 내용:\n{}", contents);
+}
