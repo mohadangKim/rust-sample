9763f61a692b4f6bb8e31208dc7cce92a13ca7c5
mohadangKim <ehdlaos@gmail.com>
2022-11-26 20:39:22 +0900

    리팩토링 : 잘못된 인자 전달받을 경우 panic! 대신 Result 객체 반환
    - 잘못된 인자 전달될 경우 사용자에게 필요 없는 에러 메시지 내용을 제거

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,9 +1,15 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
-  let config = Config::new(&args);
+  let config = Config::new(&args).unwrap_or_else(|err| {
+    // 반환이 Ok이면 unwrap과 같이 동작함, 즉 Ok 값을 unwrap 하여 결과값을 반환
+    // 반환이 Err이면 클로저를 이용해 unwrap_or_else에 전달한 익명 함수를 호출
+    println!("인수를 구문분석하는 동안 오류가 발생했습니다.: {}", err);
+    process::exit(1);
+  });
   
   println!("검색어 : {}", config.query);
   println!("대상 파일 : {}", config.filename);
@@ -18,13 +24,13 @@ struct Config {
 }
 
 impl Config {
-  fn new(args: &[String]) -> Config {
+  fn new(args: &[String]) -> Result<Config, &'static str> {
     if args.len() < 3 {
-      panic!("필요한 인수가 지정되지 않았습니다.");
+      return Err("필요한 인수가 지정되지 않았습니다.");
     }
     let query = args[1].clone();
     let filename = args[2].clone();
   
-    Config { query, filename }
+    Ok(Config { query, filename })
   }  
 }
