d54080211426241c4bdb787599cbb6c714ac83fe
mohadangKim <ehdlaos@gmail.com>
2022-12-04 17:52:47 +0900

    리팩토링 : 루프 작성 코드를 반복 대체

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -51,14 +51,9 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   //수명 어노테이션 사용하여 인수의 수명이 리턴값의 수명과 연결 되었음을 컴파일러에 알림
   //contents 인수는 검색 대상이 되는 모든 텍스트를 저장하고 있으며
   //리턴할 값은 그 일부이므로 contents 인수의 수명이 리턴값의 수명과 연결되어야 한다.
-  let mut results = Vec::new();
-  for line in contents.lines() {
-    if line.contains(query) {
-      results.push(line);
-    }
-  }
-
-  results
+  contents.lines()
+    .filter(|line| line.contains(query))
+    .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
